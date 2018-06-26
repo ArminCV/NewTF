4ae0a1102feddd25d5f57259c057dda646c601f3
armincvaldez <armincvaldez@gmail.com>
2018-06-26 20:20:51 +0800

    Initial commit

---
+++ app/src/main/java/com/example/armin/newtf/MainMenu.java
@@ -0,0 +1,14 @@
+package com.example.armin.newtf;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+
+public class MainMenu {
+    private static final int CAMERA_REQUEST = 100;
+
+
+}
